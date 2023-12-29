99642f5a86290d091731a545775221f8024f5e9e
HaiLH215 <hoaihai.fw@gmail.com>
2023-12-29 15:59:13 +0700

    [UI] update

---
+++ HVPPCafeDesktop/Views/SubViews/ChamCong.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HVPPCafeDesktop.Views.SubViews
+{
+    /// <summary>
+    /// Interaction logic for ChamCong.xaml
+    /// </summary>
+    public partial class ChamCong : Window
+    {
+        public ChamCong()
+        {
+            InitializeComponent();
+        }
+
+        private void Label_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+    }
+}
